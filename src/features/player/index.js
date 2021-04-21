ba9831b8b8ab34562d20d5f48a2c64d9a965cd33
mike-parkin <michael.parkin1988@gmail.com>
2021-04-21 15:52:34 +1200

    fixed player component typo, image now shows on screen

--- src/features/player/index.js
+++ src/features/player/index.js
@@ -7,8 +7,8 @@ const Player = (props) => {
         <div
             style={{
                 postion: 'relative',
-                top: props.postion[1],
-                left: props.postion[0],
+                top: props.position[1],
+                left: props.position[0],
                 backgroundImage: `url('${walkSprite}')`,
                 backgroundPosition: '0 0',
                 width: '32px',
